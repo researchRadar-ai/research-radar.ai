04c300e3f8a2d23bd4f15b99f873e73b6d3ff178
drkchn <57195399+drkchn@users.noreply.github.com>
2023-09-09 13:54:36 -0400

    Extracting text from JSON returned file

--- src/metaphor-api/metaphor.py
+++ src/metaphor-api/metaphor.py
@@ -1,14 +1,16 @@
 import datetime
 import os
+import yake
 
 from dotenv import load_dotenv
 from metaphor_python import Metaphor
+from collections import defaultdict
 
 
 def main():
     load_dotenv()
     METAPHOR_API_KEY = os.getenv('METAPHOR_API_KEY')
-    client = Metaphor(api_key=METAPHOR_API_KEY)
+    metaphor = Metaphor(api_key=METAPHOR_API_KEY)
 
     user = input("What are you looking for? ")
     print()
@@ -17,14 +19,25 @@ def main():
     today = datetime.date.today()
     one_year_ago = today - datetime.timedelta(days=365)
 
-    response = client.search(user, num_results=10, 
+    response = metaphor.search(user, num_results=10, 
                             include_domains=["scholar.google.com", "jstor.org", "ncbi.nlm.nih.gov", "springer.com", "ieeexplore.ieee.org", "sciencedirect.com", "onlinelibrary.wiley.com", "nature.com", "pnas.org", "arxiv.org", "frontiersin.org", "dl.acm.org"],
                             start_published_date=str(one_year_ago), 
                             use_autoprompt=True,)
 
+    document_dict = defaultdict(str)
     for result in response.results:
         print(result.title, result.url, result.id)
         print()
+
+        document_dict[result.title] = result.id
+
+    print(document_dict.items())
+
+    get_keywords("mVcN4TkCWpvQ2g-9hxnI7Q", metaphor)
+
+def get_keywords(document_id, metaphor):
+    response = metaphor.get_contents(document_id)
+    print(response.contents[0].extract)
 
 
 if __name__ == '__main__':
