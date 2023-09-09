ba5be180d9030d3e0cd7780f40448825ae7b6694
drkchn <57195399+drkchn@users.noreply.github.com>
2023-09-09 16:08:48 -0400

    Added comments

--- src/metaphor-api/metaphor.py
+++ src/metaphor-api/metaphor.py
@@ -8,39 +8,50 @@ from collections import defaultdict
 
 
 def main():
+    # load the Metaphor API key from .env file
     load_dotenv()
     METAPHOR_API_KEY = os.getenv('METAPHOR_API_KEY')
     metaphor = Metaphor(api_key=METAPHOR_API_KEY)
 
+    # user inputs phrase they want to search
     user = input("What are you looking for? ")
     print()
     print("Here are your results!")
 
+    # grab the most relevant results
+    # Metaphor API will return results that are within one year of the publish date
     today = datetime.date.today()
     one_year_ago = today - datetime.timedelta(days=365)
 
+    # Metaphor API call
     response = metaphor.search(user, num_results=10, 
                             include_domains=["scholar.google.com", "jstor.org", "ncbi.nlm.nih.gov", "springer.com", "ieeexplore.ieee.org", "sciencedirect.com", "onlinelibrary.wiley.com", "nature.com", "pnas.org", "arxiv.org", "frontiersin.org", "dl.acm.org"],
                             start_published_date=str(one_year_ago), 
                             use_autoprompt=True,)
 
+    # create a hashmap to store
+    # key = article title
+    # value = article id
     document_dict = defaultdict(str)
     for result in response.results:
-        print(result.title, result.url, result.id)
-        print()
+        #print(result.title, result.url, result.id)
+        #print()
 
         document_dict[result.title] = result.id
         keywords = get_keywords(str(document_dict[result.title]), metaphor)
-        return keywords
+        print(keywords)
 
     #print(document_dict.items())
 
     
 
 def get_keywords(document_id, metaphor):
+
+    # Metaphor API call
     response = metaphor.get_contents(document_id)
     text = response.contents[0].extract
 
+    # YAKE custom settings
     language = "en"
     max_ngram_size = 3
     deduplication_thresold = 0.9
@@ -48,6 +59,7 @@ def get_keywords(document_id, metaphor):
     windowSize = 1
     numOfKeywords = 10
 
+    # YAKE keyword extractor
     kw_extractor = yake.KeywordExtractor(lan=language, 
                                     n=max_ngram_size, 
                                     dedupLim=deduplication_thresold, 
@@ -57,11 +69,14 @@ def get_keywords(document_id, metaphor):
                                             
     keywords = kw_extractor.extract_keywords(text)
 
-    important_words = []
+    # list to store the keywords
+    keywords_lst = []
     for kw, score in keywords:
-        important_words.append(kw)
+        if kw not in ["div", "Download PDF", "pdf", "Download", "href"]:
+            keywords_lst.append(kw)
 
-    return important_words
+    #print(important_words)
+    return keywords_lst
 
 
 if __name__ == '__main__':
